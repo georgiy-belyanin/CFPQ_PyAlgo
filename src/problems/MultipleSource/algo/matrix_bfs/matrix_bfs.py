a0bc07343af7ceb24d6083f08341dc6188842fb6
Georgiy Belyanin <belyaningeorge@ya.ru>
2024-05-17 14:07:12 +0300

    regular: add 2-RPQs support

--- src/problems/MultipleSource/algo/matrix_bfs/matrix_bfs.py
+++ src/problems/MultipleSource/algo/matrix_bfs/matrix_bfs.py
@@ -10,6 +10,7 @@ from pygraphblas.matrix import Matrix
 from pygraphblas.vector import Vector
 from pygraphblas import descriptor
 from pygraphblas import Accum, binaryop
+import pygraphblas
 
 from src.graph.graph import Graph
 from src.problems.MultipleSource.algo.matrix_bfs.reg_automaton import RegAutomaton
@@ -29,8 +30,13 @@ class MatrixIntersectionBfs:
         """
         inverse_matrices = {}
         for symbol in self.regular_automaton.matrices:
-            inverse_matrices[symbol] = self.regular_automaton.matrices[symbol].T.dup()
+            if str(symbol).startswith('^'):
+              inverse_matrices[str(symbol)[1:]] = self.regular_automaton.matrices[symbol].T.dup()
+            else:
+              inverse_matrices[str(symbol)] = self.regular_automaton.matrices[symbol].T.dup()
 
+        for symbol in inverse_matrices:
+            inverse_matrices[symbol].format = pygraphblas.lib.GxB_BY_COL
         return inverse_matrices
 
     def get_final_states_matrix(self) -> Matrix:
@@ -68,28 +74,38 @@ class MatrixIntersectionBfs:
         inverse_matrices = self.get_inverse_matrices()
 
         result = Matrix.sparse(BOOL, 1, graph_vert_count)
+
         next_traversal_matrix = self.get_initial_traversal_matrix(src_verts)
+        next_visited_matrix = next_traversal_matrix.dup()
+
         traversal_matrix = Matrix.sparse(BOOL, regex_vert_count, graph_vert_count)
+        visited_matrix = Matrix.sparse(BOOL, regex_vert_count, graph_vert_count)
+
 
         # Algo's body
         not_empty = True
         while not_empty:
-            not_empty_for_at_least_one_symbol = False
+            not_empty_for_at_least_one_symbol = True
 
             traversal_matrix.assign_matrix(next_traversal_matrix)
+            visited_matrix.assign_matrix(next_visited_matrix)
             next_traversal_matrix.clear()
 
             # stores found nodes for each symbol
 
             for symbol in regex:
                 with BOOL.ANY_PAIR:
-                    with Accum(binaryop.MAX_BOOL):
-                        inverse_matrices[symbol].mxm(traversal_matrix.mxm(graph[symbol]), out=next_traversal_matrix)
-            if not next_traversal_matrix.iseq(traversal_matrix):
-                not_empty_for_at_least_one_symbol = True
+                    #if str(symbol).startswith('^'):
+                    #    inverse_matrices[str(symbol)[1:]].mxm(traversal_matrix.mxm(graph[str(symbol)[1:]], desc=descriptor.T1), out=next_traversal_matrix, mask=next_traversal_matrix, desc=descriptor.C)
+                    #else:
+                    inverse_matrices[symbol].mxm(traversal_matrix.mxm(graph[symbol]), out=next_traversal_matrix, mask=next_traversal_matrix, desc=descriptor.C & descriptor.S)
+
+            next_visited_matrix.assign_matrix(next_traversal_matrix, mask=next_visited_matrix, desc=descriptor.C & descriptor.S)
+            if visited_matrix.iseq(next_visited_matrix):
+                not_empty_for_at_least_one_symbol = False
 
             with Accum(binaryop.MAX_BOOL):
-                final_states_matrix.mxm(traversal_matrix, out=result)
+                final_states_matrix.mxm(next_traversal_matrix, out=result)
 
             not_empty = not_empty_for_at_least_one_symbol
 
